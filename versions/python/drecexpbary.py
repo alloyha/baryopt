f38a036786fe8cc23c54e0bd163f13eadba09d51
Astolfo <brunolnetto@gmail.com>
2022-02-20 12:12:47 -0300

    feat/examples on notebook and raw python

--- versions/python/drecexpbary.py
+++ versions/python/drecexpbary.py
@@ -1,52 +1,49 @@
 def drecexpbary(oracle, x0,
                 nu = 1, sigma = 0.1, 
                 zeta = 0, lambda_ = 1, 
-                iterations = 100):
+                iterations = 1000):
     '''
      Recursive barycenter algorithm for direct optimization
+     
      In:
-       - oracle [function]: Oracle function e.g. lambda(x) numpy.power(x, 2)
-       - x0 [np.array]: Initial query values
-       - nu [double]: positive value (Caution on its value due overflow)
-       - sigma [double]: Std deviation of normal distribution
-
-        - zeta [double]: Proportional value for mean of normal distribution
-        - lambda [double]: Forgetting factor between 0 and 1
-        - iterations [int]: Maximum number of iterations
+       - oracle     [function]  : Oracle function e.g. lambda x: numpy.power(x, 2)
+       - x0         [np.array]  : Initial query values
+       - nu         [double]    : positive value (Caution on its value due overflow)
+       - sigma      [double]    : Std deviation of normal distribution
+       - zeta       [double]    : Proportional value for mean of normal distribution
+       - lambda     [double]    : Forgetting factor between 0 and 1
+       - iterations [int]       : Maximum number of iterations
+     
      Out:
         - x [np.array]: Optimum position
     '''
     import numpy as np
     import scipy as scp
-
+    
     # Initialization
     xhat_1 = x0
     m_1 = 0
 
-    len_x0 = x0.__len__()
-    deltax_1 = np.zeros((len_x0, 1))
-    solution_found = False
-
+    deltax_1 = np.zeros((len(x0), 1))
+    solution_is_found = False
     normrnd = scp.random.normal
-
+    
     # Optimization loop
     i = 1
-    while(not solution_found):
-        i = i + 1
-
+    while(not solution_is_found):    
         z = normrnd(zeta*deltax_1, sigma).T
         
         x = xhat_1 + z
-        fi = oracle(x)
-
-        e_i = np.exp(-nu*fi)
-        
+        e_i = np.exp(-nu*oracle(x))
         m = lambda_*m_1 + e_i
-        
-        m_1=m
-
         xhat = (1/m)*(lambda_*m_1*xhat_1 + x*e_i)
         
-        solution_found = i >= iterations
+        solution_is_found = i >= iterations
         
+        # Update previous variables
+        m_1=m
+        xhat_1=xhat
+        
+        i = i + 1
+    
     return xhat
--- versions/python/examples/main.py
+++ versions/python/examples/main.py
@@ -1,7 +1,8 @@
-if __name__ == "__main__":
-    import numpy as np
+from drecexpbary import drecexpbary
+import numpy as np
 
-    oracle = lambda(x): np.power(x, 2)
+if __name__ == "__main__":
+    oracle = lambda x: np.power(x, 2)
     x0 = np.array([0, 0])
     x = drecexpbary(oracle, x0, 10, 0.1, 0, 1, 100)
     
